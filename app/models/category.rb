80a87295df263922e9a91213bff072607c144e69
Jo Jangmi <wkdal720@gmail.com>
2016-08-31 14:18:31 -0700

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   has_one :image, as: :imageable
+  has_many :recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :images, as: :imageable
+  has_many :steps
+  has_many :videos
+  belongs_to :category
 end
