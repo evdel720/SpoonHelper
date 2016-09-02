3dbaa80d7ec46e2e8a547e69eabced17b2695b20
Jo Jangmi <wkdal720@gmail.com>
2016-09-02 10:34:49 -0700

    fixed bug

--- frontend/components/recipe/recipe_index_item.jsx
+++ frontend/components/recipe/recipe_index_item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RecipeIndexItem = ({ item }) => (
   <li className="recipe-index-item">
     <h4>{item.title}</h4>
-    <p>item.user.email</p>
+    <p>{ item.user }</p>
   </li>
 );
 
